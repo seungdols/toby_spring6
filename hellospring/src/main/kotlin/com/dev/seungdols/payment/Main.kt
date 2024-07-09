9c8c9be94e6d3b9c621bc867691e773b0b028887
seungdols <seungdols0822@gmail.com>
2024-07-09 20:36:09 +0900

    feat: 관계설정 책임의 분리

--- hellospring/src/main/kotlin/com/dev/seungdols/payment/Main.kt
+++ hellospring/src/main/kotlin/com/dev/seungdols/payment/Main.kt
@@ -1,10 +1,11 @@
 package com.dev.seungdols.payment
 
 import com.dev.seungdols.payment.service.PaymentService
+import com.dev.seungdols.payment.service.WebApiExRateProvider
 import java.math.BigDecimal
 
 fun main() {
-  val paymentService = PaymentService()
+  val paymentService = PaymentService(WebApiExRateProvider())
   val payment = paymentService.prepare(100L, "USD", BigDecimal("100.00"))
   println(payment)
 }
--- hellospring/src/main/kotlin/com/dev/seungdols/payment/service/PaymentService.kt
+++ hellospring/src/main/kotlin/com/dev/seungdols/payment/service/PaymentService.kt
@@ -5,7 +5,7 @@ import java.math.BigDecimal
 import java.time.LocalDateTime
 
 class PaymentService(
-  private val exRateProvider: ExRateProvider = WebApiExRateProvider(),
+  private val exRateProvider: ExRateProvider,
 ) {
   fun prepare(
     orderId: Long,
